bfc8172bfc1a48b511ca1562ce87e930ecbf452f
Przemek Soból <przemyslaw.sobol@gmail.com>
2020-07-24 19:01:49 +0200

    Add lista_zakupów dict

--- zakupy.py
+++ zakupy.py
@@ -1,16 +1,3 @@
 lista_zakupów = {}
 lista_zakupów["mediamarkt"] = ["ps4", "tablet", "słuchawki"]
 lista_zakupów["decathlon"] = ["rower", "kurtka", "buty"]
-print(lista_zakupów)
-
-for sklep, produkt in lista_zakupów:
-    sklepy = str(sklep)
-    supersklepy = sklepy.title()
-    produkty = str(produkt)
-    superprodukty = produkty.title()
-    zakupy = f"Idę do {supersklepy} i kupuję tam {superprodukty}"
-    print(zakupy)
-
-    wszystkie_zakupy = lista_zakupów["mediamarkt"] + lista_zakupów["decathlon"]
-    suma_zakupow = len(wszystkie_zakupy)
-    print("W sumie kupiłem {} produktów".format(suma_zakupow))
