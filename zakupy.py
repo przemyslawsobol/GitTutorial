3451e65d83f17801e091c529ae7217dbc9ea6ccd
Przemek Soból <przemyslaw.sobol@gmail.com>
2020-07-24 19:08:04 +0200

    Add loop for print text

--- zakupy.py
+++ zakupy.py
@@ -1,3 +1,11 @@
 lista_zakupów = {}
 lista_zakupów["mediamarkt"] = ["ps4", "tablet", "słuchawki"]
 lista_zakupów["decathlon"] = ["rower", "kurtka", "buty"]
+
+
+for sklep, produkt in lista_zakupów.items():
+    sklepy = str(sklep)
+    supersklepy = sklepy.title()
+    produkty = str(produkt)
+    superprodukty = produkty.title()
+    print(f"Idę do {supersklepy} i kupuję tam {superprodukty}")
